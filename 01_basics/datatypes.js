d616077961fe03182fb000999e921d4784851d35
Sushant Ravva <113240987+SushantRavva@users.noreply.github.com>
2024-01-06 14:49:05 +0000

    about datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+4) // this will not work as we are using nodeJS not browser
+
+// number  => 2 raised to 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
